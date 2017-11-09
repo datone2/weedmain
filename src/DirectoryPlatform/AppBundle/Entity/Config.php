4d1122fcb16d9539d6dfdf25c83151835162b344
Achref Ben Dhia <Achref Ben Dhia>
2017-11-09 20:56:13 +0100

    listings+ admin distance config

---
+++ src/DirectoryPlatform/AppBundle/Entity/Config.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace DirectoryPlatform\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Config
+ *
+ * @ORM\Table(name="config")
+ * @ORM\Entity(repositoryClass="DirectoryPlatform\AppBundle\Repository\ConfigRepository")
+ */
+class Config
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="distance", type="float", nullable=true)
+     */
+    private $distance;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set distance
+     *
+     * @param float $distance
+     *
+     * @return Config
+     */
+    public function setDistance($distance)
+    {
+        $this->distance = $distance;
+
+        return $this;
+    }
+
+    /**
+     * Get distance
+     *
+     * @return float
+     */
+    public function getDistance()
+    {
+        return $this->distance;
+    }
+}
+
